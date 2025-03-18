41b239214c003f121a4fc5c3655bece8e0f53374
SHZTD <tdrag@elpuig.xeill.net>
2025-03-18 23:49:46 +0100

    Initial commit

---
+++ app/src/main/java/com/example/socialappuf2/MediaFragment.java
@@ -0,0 +1,59 @@
+package com.example.socialappuf2;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.bumptech.glide.Glide;
+
+import org.jetbrains.annotations.Nullable;
+
+public class MediaFragment extends Fragment {
+    VideoView videoView;
+    ImageView imageView;
+    public AppViewModel appViewModel;
+
+    public MediaFragment() {
+// Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup
+            container, Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_media, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle
+            savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        appViewModel = new ViewModelProvider(requireActivity()).get(AppViewModel.class);
+        imageView = view.findViewById(R.id.imageView);
+        videoView = view.findViewById(R.id.videoView);
+        appViewModel.postSeleccionado.observe(getViewLifecycleOwner(), post
+                -> {
+            String mediaType = post.get("mediaType").toString();
+            String mediaUrl = post.get("mediaUrl").toString();
+            if ("video".equals(mediaType) ||
+                    "audio".equals(mediaType)) {
+                MediaController mc = new
+                        MediaController(requireContext());
+                mc.setAnchorView(videoView);
+                videoView.setMediaController(mc);
+                videoView.setVideoPath(post.get("mediaUrl").toString());
+                videoView.start();
+            } else if ("image".equals(mediaType)) {
+                Glide.with(requireView()).load(mediaUrl).into(imageView);
+            }
+        });
+    }
+}
